80a207dd82874b0e85aecb03735cff4e677fa498
Randy <o1984531@gmail.com>
2024-02-10 14:36:27 +0800

    (TextField 3.) --- onSubmitted notify edit finished

---
+++ lib/text_field/SubmitTextField.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SubmitTextField extends StatefulWidget {
+  const SubmitTextField({super.key});
+
+  @override
+  State<SubmitTextField> createState() => _SubmitTextFieldState();
+}
+
+class _SubmitTextFieldState extends State<SubmitTextField> {
+  String _inputStr = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('SubmitTextField'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text("$_inputStr"),
+            TextField(
+              onSubmitted: (value) {
+                setState(() {
+                  _inputStr = value;
+                });
+              },
+            )
+          ],
+        ));
+  }
+}
