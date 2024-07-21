a20db651fa53be3ff6e0c1b068d082ca3892d045
Randy <o1984531@gmail.com>
2024-07-21 14:32:38 +0800

    (ScaffoldMessenger 1.) ScaffoldMessenger sample 1

---
+++ lib/scaffold_messenger/ScaffoldMessengerTrigger.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldMessengerTrigger extends StatefulWidget {
+  const ScaffoldMessengerTrigger({super.key});
+
+  @override
+  State<ScaffoldMessengerTrigger> createState() =>
+      _ScaffoldMessengerTriggerState();
+}
+
+class _ScaffoldMessengerTriggerState extends State<ScaffoldMessengerTrigger> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ScaffoldMessengerTrigger'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(SnackBar(
+                content: const Text('SnackBar message'),
+                action: SnackBarAction(
+                  label: "Undo",
+                  onPressed: () {
+                    debugPrint("Undo pressed");
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                  },
+                ),
+              ));
+          },
+          child: Icon(Icons.message),
+        ));
+  }
+}
