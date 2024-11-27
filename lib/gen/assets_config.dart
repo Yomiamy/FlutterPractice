1dc37930c1eaf863ee31a6b0476884e0e0698d13
randyh <randyh@cnct.co>
2024-11-28 02:04:02 +0800

    Add a extension imageFile property to access Image

--- lib/gen/assets_config.dart
+++ lib/gen/assets_config.dart
@@ -1,5 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/image.dart';
 import 'package:flutter_practice/gen/colors.gen.dart';
 import '../gen/assets.gen.dart';
 
 typedef AssetColorRes = ColorName;
+
 $AssetsImagesGen AssetImageRes = Assets.images;
+
+extension AssetGenImageExtension on AssetGenImage {
+  Image get imageFile => image();
+}
