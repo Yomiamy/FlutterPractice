db44561c17461f5cdc0f62abd4c7bfadbf9b3849
randyh <randyh@cnct.co>
2024-12-09 00:40:48 +0800

    Add the audio player

--- lib/homework/Practice_168.dart
+++ lib/homework/Practice_168.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/gen/assets_config.dart';
 
@@ -15,11 +16,11 @@ class _Practice168State extends State<Practice168> {
         "https://onair.family977.com.tw:8977/live.mp3",
         "FM 97.7\n在台北地區，FM 97.7由台灣聲音廣播電台播出，成立於2005年，主要服務基隆市、台北市和新北市。該電台以24小時播放精選的台語老歌、國語老歌和懷舊日本老歌為特色，深受聽眾喜愛，迅速建立了忠實的聽眾群。"),
     AudioInfo(AssetImageRes.icRadio2.imageFile,
-        "https://onair.bravo913.com.tw:9130/live.mp3 ",
+        "http://fm901.cityfm.tw:8080/901.mp3 ",
         "Bravo 91.3\nBravo FM91.3（台北都會音樂台）於2009年3月開播，是台北地區的音樂廣播電台，主要播放古典音樂、爵士樂等多元音樂類型，並提供藝文資訊，致力於提升聽眾的音樂素養和生活品味。該電台與好家庭廣播FM97.7合作，組成「好家庭聯播網」，共同推廣優質音樂與文化內容。"),
     AudioInfo(AssetImageRes.icRadio3.imageFile,
-        "https://stream.rcs.revma.com/pntx1639ntzuv.m4a",
-        "News 98\n「News 98」，全名「九八新聞台」，於1999年9月8日在大台北地區開播，頻率為FM 98.1。這是全國唯一以新聞為主體的廣播電台，提供即時新聞、財經資訊和多元節目，讓聽眾隨時掌握最新資訊"),
+        "http://stream.pbs.gov.tw:1935/live/TPS/playlist.m3u8",
+        "警察廣播電臺（簡稱警廣）是中華民國的國營廣播電臺，隸屬於內政部警政署，成立於1954年3月1日，是全國首個公營全區廣播電臺"),
     AudioInfo(
         AssetImageRes.icRadio4.imageFile,
         "https://stream.ginnet.cloud/live0130lo-yfyo/_definst_/fm/playlist.m3u8",
@@ -67,8 +68,30 @@ class AudioDetail extends StatefulWidget {
 }
 
 class _AudioDetailState extends State<AudioDetail> {
+
+  late AudioPlayer _player;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _player = AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _player.dispose();
+  }
+
+  _playAudio() async {
+    await _player.play(UrlSource(widget._audioInfo.audioUrl), mode: PlayerMode.mediaPlayer);
+  }
+
   @override
   Widget build(BuildContext context) {
+    _playAudio();
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("AudioDetail"),
