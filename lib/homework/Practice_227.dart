a8e6635c1023d18fad37dffe550e30f810b89f09
randyh <randyh@cnct.co>
2024-12-16 01:43:31 +0800

    Add answer button and change next quiz

--- lib/homework/Practice_227.dart
+++ lib/homework/Practice_227.dart
@@ -1,9 +1,9 @@
-
-
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_practice/gen/assets_config.dart';
 import 'package:flutter_practice/homework/Practice_227API.dart';
+import 'package:flutter_practice/homework/Quiz.dart';
 
 class Practice227 extends StatefulWidget {
   const Practice227({super.key});
@@ -15,49 +15,32 @@ class Practice227 extends StatefulWidget {
 class _Practice227State extends State<Practice227> {
   @override
   Widget build(BuildContext context) {
-    practice_227APIClient.getQuestionInfo(10, "boolean").then((quiz) {
-      debugPrint("");
-    }).catchError((error) {
-      switch(error.runtimeType) {
-        case DioException _:
-          final res = (error as DioException).response;
-          debugPrint('Got error : ${res?.statusCode} -> ${res?.statusMessage}');
-          break;
-        default:
-          break;
-      }
-    });
-    
-    
     return CupertinoPageScaffold(
         navigationBar: const CupertinoNavigationBar(
           padding: EdgeInsetsDirectional.zero,
           middle: Text("Practice227"),
         ),
-        child: SafeArea(child: Container(
-          padding: const EdgeInsets.fromLTRB(15, 40, 15, 20),
-          width: MediaQuery.sizeOf(context).width,
-          height: MediaQuery.sizeOf(context).height,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImageRes.classroom.provider(),
-                fit: BoxFit.fill
-            )
-          ),
-          child: Column(
-            children: [
-              AssetImageRes.openlight.imageFile,
-              CupertinoButton(
-                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-                  color: CupertinoColors.activeGreen,
-                  child: const Text('START'),
-                  onPressed: () {
-                    Navigator.of(context).push(CupertinoPageRoute(builder: (context) => QuizPage()));
-                  }
-              )
-            ]
-          )
-        )));
+        child: SafeArea(
+            child: Container(
+                padding: const EdgeInsets.fromLTRB(15, 40, 15, 20),
+                width: MediaQuery.sizeOf(context).width,
+                height: MediaQuery.sizeOf(context).height,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImageRes.classroom.provider(),
+                        fit: BoxFit.fill)),
+                child: Column(children: [
+                  AssetImageRes.openlight.imageFile,
+                  CupertinoButton(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 50),
+                      color: CupertinoColors.activeGreen,
+                      child: const Text('START'),
+                      onPressed: () {
+                        Navigator.of(context).push(CupertinoPageRoute(
+                            builder: (context) => QuizPage()));
+                      })
+                ]))));
   }
 }
 
@@ -69,15 +52,92 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int _quizIndex = 0;
+  set index(int newIndex) {
+    _quizIndex = newIndex;
+  }
+  int get index {
+    return _quizIndex;
+  }
+
+  late Quiz _quiz;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: const Text("QuizPage"),
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text("QuizPage"),
         ),
-        child: Container(
-          child: const Text("QuizPage")
-        ));
+        child: FutureBuilder(
+            future: fetchQuiz(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                _quiz = snapshot.data!;
+                return SafeArea(
+                    child: StatefulBuilder(builder: (context, setState) {
+                  return Container(
+                      padding: const EdgeInsets.fromLTRB(15, 40, 15, 20),
+                      width: MediaQuery.sizeOf(context).width,
+                      height: MediaQuery.sizeOf(context).height,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImageRes.classroom.provider(),
+                              fit: BoxFit.fill)),
+                      child: Column(children: [
+                        Text("Q - ${_quizIndex}",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: CupertinoColors.white,
+                                fontSize: 26)),
+                        const SizedBox(height: 40),
+                        Text(
+                            "${_quiz.results?[_quizIndex].question}",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: CupertinoColors.white,
+                                fontSize: 24)),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                                width: 50,
+                                height: 50,
+                                child: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        _quizIndex = (_quizIndex <
+                                                _quiz.results!.length - 1)
+                                            ? ++_quizIndex
+                                            : _quizIndex;
+                                      });
+                                    },
+                                    child: AssetImageRes.o.imageFile)),
+                            const SizedBox(width: 20),
+                            SizedBox(
+                                width: 50,
+                                height: 50,
+                                child: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        _quizIndex = (_quizIndex <
+                                                _quiz.results!.length - 1)
+                                            ? ++_quizIndex
+                                            : _quizIndex;
+                                      });
+                                    },
+                                    child: AssetImageRes.x.imageFile))
+                          ],
+                        )
+                      ]));
+                }));
+              } else {
+                return const SafeArea(
+                    child: Center(child: CircularProgressIndicator()));
+              }
+            }));
   }
-}
 
+  Future<Quiz> fetchQuiz() =>
+      practice_227APIClient.getQuestionInfo(10, "boolean");
+}
