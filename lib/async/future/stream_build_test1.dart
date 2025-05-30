891033e45145546ff7e1f8aadcae13754b959160
Yianday <yianday0330h@gmail.com>
2025-05-30 12:46:19 +0800

    Add StreamBuildTest1

---
+++ lib/async/future/stream_build_test1.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class StreamBuildTest1 extends StatefulWidget {
+  const StreamBuildTest1({super.key});
+
+  @override
+  State<StreamBuildTest1> createState() => _StreamBuildTest1State();
+}
+
+class _StreamBuildTest1State extends State<StreamBuildTest1> {
+
+  Stream<String> mockFetchData() {
+    return Stream.periodic(Duration(seconds: 2), (i) {
+      if (i >= 5) {
+        throw "Stream completed manually.";
+      }
+      return "mockData $i";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('StreamBuildTest1'),
+        ),
+        body: StreamBuilder(
+            stream: mockFetchData(),
+            builder: (context, snapshot) {
+              if(snapshot.hasError) {
+                return Text("Error: ${snapshot.error}");
+              }
+
+              if(snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("等待數據....");
+              } else if(snapshot.connectionState == ConnectionState.active) {
+                return Text('Data: ${snapshot.data}');
+              } else if(snapshot.connectionState == ConnectionState.done) {
+                return const Text('Stream關閉...');
+              } else {
+                return Container(
+                  alignment: Alignment.center,
+                  child: const CircularProgressIndicator(),
+                );
+              }
+            }
+        ));
+  }
+}
