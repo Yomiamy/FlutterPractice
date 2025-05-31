2a6421b32828c041814008668531699d845b7c57
Yianday <yianday0330h@gmail.com>
2025-05-31 22:44:42 +0800

    DatePickerDialogTest1

---
+++ lib/dialog/date_picker_dialog_test1.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DatePickerDialogTest1 extends StatefulWidget {
+  const DatePickerDialogTest1({super.key});
+
+  @override
+  State<DatePickerDialogTest1> createState() => _DatePickerDialogTest1State();
+}
+
+class _DatePickerDialogTest1State extends State<DatePickerDialogTest1> {
+  @override
+  Widget build(BuildContext context) {
+    DateTime selectedDate = DateTime.now();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("DatePickerDialogTest1"),
+        ),
+        body: ElevatedButton(
+            onPressed: () {
+              showDatePicker(
+                  context: context,
+                  firstDate: selectedDate,
+                  lastDate: selectedDate.add(Duration(days: 30)
+                  )
+              );
+            },
+            child: const Text("Date Picker")));
+  }
+}
