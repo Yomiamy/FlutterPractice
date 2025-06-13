ecb748ab535615fb34c98ad4b79b06b5bf1e5686
Yianday <yianday0330h@gmail.com>
2025-06-13 21:36:06 +0800

    Add TextFieldTest1

---
+++ lib/form/text_field_test1.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+
+class TextFieldTest1 extends StatefulWidget {
+  const TextFieldTest1({super.key});
+
+  @override
+  State<TextFieldTest1> createState() => _TextFieldTest1State();
+}
+
+class _TextFieldTest1State extends State<TextFieldTest1> {
+
+  final TextEditingController _controller = TextEditingController.fromValue(const TextEditingValue(text: "Init Value"));
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller.addListener(() {
+      debugPrint("Input: ${_controller.text}");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("TextFieldTest1"),
+        ),
+        body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0), // 調整水平邊距以控制長度
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'Enter a string',
+              ),
+              // onChanged: (input) {
+              //   debugPrint("Input: $input");
+              // },
+            ),
+          )
+      ]
+      ),
+    );
+  }
+}
