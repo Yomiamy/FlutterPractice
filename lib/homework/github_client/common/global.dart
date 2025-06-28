e045d42579d339f3121ca57b122a1eb0253f3b6e
Yianday <yianday0330h@gmail.com>
2025-06-28 19:34:51 +0800

    Refactor some codes

--- lib/homework/github_client/common/global.dart
+++ lib/homework/github_client/common/global.dart
@@ -2,18 +2,21 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 import '../api/github_api_manager.dart';
 import '../models/user.dart';
 
 const _themes = <MaterialColor>[Colors.blue, Colors.cyan, Colors.teal, Colors.green, Colors.red];
 
 class Global {
-
   static const _SP_EXTRA_KEY = "user";
   static const _ENV_FLAVOR_KEY = "FLAVOR";
   static const _ENV_DEV = "dev";
   static const _ENV_BETA = "beta";
   static const _ENV_PROD = "prod";
+
+  // GitHub API Token
+  static const String GITHUB_TOKEN = "<Personal_Token>";
 
   static Global? _sInstance;
 
@@ -25,7 +28,8 @@ class Global {
   User? user;
   late SharedPreferences _prefs;
 
-  bool get isProd => const String.fromEnvironment(_ENV_FLAVOR_KEY, defaultValue: _ENV_DEV) == _ENV_PROD;
+  bool get isProd =>
+      const String.fromEnvironment(_ENV_FLAVOR_KEY, defaultValue: _ENV_DEV) == _ENV_PROD;
 
   Global._();
 
@@ -40,6 +44,6 @@ class Global {
     GithubApiManager.instance.init();
   }
 
-  void saveProfile() => _prefs.setString(_SP_EXTRA_KEY, user != null ? jsonEncode(user?.toJson()) : "");
-
+  void saveProfile() =>
+      _prefs.setString(_SP_EXTRA_KEY, user != null ? jsonEncode(user?.toJson()) : "");
 }
