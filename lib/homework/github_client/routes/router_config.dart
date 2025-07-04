f8f06840a2911f1f4cb0726580ee15e546b3403a
Yianday <yianday0330h@gmail.com>
2025-07-05 01:58:18 +0800

    Use the pageBuilder to add to add the custom page transition

--- lib/homework/github_client/routes/router_config.dart
+++ lib/homework/github_client/routes/router_config.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
 import '../widgets/github_client_login.dart';
@@ -7,5 +8,19 @@ final globalRouteConfig = GoRouter(initialLocation: GithubClientLogin.ROUTE_NAME
   GoRoute(path: GithubClientLogin.ROUTE_NAME, builder: (_, __) => const GithubClientLogin()),
   GoRoute(
       path: GithubClientRepositoryList.ROUTE_NAME,
-      builder: (_, __) => const GithubClientRepositoryList())
+      pageBuilder: (_, state) => CustomTransitionPage(
+          transitionDuration: const Duration(seconds: 1), // 指定 transition 的 duration
+          key: state.pageKey,
+          child: const GithubClientRepositoryList(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            // Start from the right, // End at the center
+            // Use a curve for smooth transition
+            return SlideTransition(
+                position: Tween<Offset>(begin: const Offset(1.0, 0.0), end: Offset.zero)
+                    .animate(CurvedAnimation(
+                  parent: animation,
+                  curve: Curves.easeInOut,
+                )),
+                child: child);
+          }))
 ]);
