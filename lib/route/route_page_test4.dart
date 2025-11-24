26ef86f01d187429efb08cce0746eaad8b4b112a
Yianday <yianday0330h@gmail.com>
2025-11-24 15:52:04 +0800

    feat(route): Add test page for route navigation
    Implement a new screen, `RoutePageTest4`, to demonstrate and test basic push navigation within the Flutter application.
    
    This new route includes a main page and a subsequent content page (`RoutePageTest4ContentPage`) to verify the navigation stack. The main application entry point (`main.dart`) has been updated to display this new test page as the initial route.

---
+++ lib/route/route_page_test4.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class RoutePageTest4 extends StatelessWidget {
+  const RoutePageTest4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('RoutePageTest4'),
+        ),
+        body: TextButton(
+          style: TextButton.styleFrom(
+            side: const BorderSide(width: 1),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const RoutePageTest4ContentPage(),
+              ),
+            );
+          },
+          child: const Text('Go Next Page'),
+        ));
+  }
+}
+
+class RoutePageTest4ContentPage extends StatelessWidget {
+  const RoutePageTest4ContentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('RoutePageTest4ContentPage'),
+        ),
+        body: const Center(
+          child: Text('RoutePageTest4ContentPage Text'),
+        ));
+  }
+}
